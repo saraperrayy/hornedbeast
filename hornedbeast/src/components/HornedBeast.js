74413538462356b66402a2ed79362dd43c75d22a
SARA PERRY <SARAPERRYY@GMAIL.COM>
2021-07-22 20:40:08 -0500

    feature 3 completed

--- hornedbeast/src/components/HornedBeast.js
+++ hornedbeast/src/components/HornedBeast.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BsFillHeartFill } from 'react-icons/bs'
+import Button from 'react-bootstrap/Button';
+import { Container, Row, Col } from 'react-bootstrap';
+import Image from 'react-bootstrap/Image'
 const { useState } = React;
 
 
@@ -12,21 +15,55 @@ function HornedBeast({image_url, title, description}) {
 
 
     return (
-    <>
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <button>
-        <img 
-        height="500px" 
-        src={image_url} 
-        alt="Parrot" 
-        onClick={handleClick}
-        title="Parrot">
-        </img>
-        <BsFillHeartFill/>
-        {counter}
-      </button>
-      </>
+      <Container>
+        <Row>
+          <Col>
+            <Button className="bg-transparent border-0">
+              <Image
+              src={image_url}
+              onClick={handleClick}
+              className="img-fluid"/>
+            </Button>
+          </Col>
+
+          <Col xs={12}>
+            <Col>
+            <BsFillHeartFill/>
+            </Col>
+
+            <Col>
+            {counter}
+            </Col>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12}>
+            <h2>
+              {title}
+            </h2>
+          </Col>
+          <Col>
+            <p>
+              {description}
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    // <>
+    //   <h2>{title}</h2>
+    //   <p>{description}</p>
+    //   <button>
+    //     <img 
+    //     height="500px" 
+    //     src={image_url} 
+    //     alt="Parrot" 
+    //     onClick={handleClick}
+    //     title="Parrot">
+    //     </img>
+    //     <BsFillHeartFill/>
+    //     {counter}
+    //   </button>
+    //   </>
     )
 }
 export default HornedBeast;
