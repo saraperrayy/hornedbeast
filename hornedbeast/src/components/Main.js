4cfde309c3ac195b518400b05bca137899bf4418
SARA PERRY <SARAPERRYY@GMAIL.COM>
2021-08-09 14:28:20 -0500

    lab03

--- hornedbeast/src/components/Main.js
+++ hornedbeast/src/components/Main.js
@@ -1,32 +1,32 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import HornedBeast from './HornedBeast';
-import { animalData } from '../data';
+// import SelectedBeast from './SelectedBeast';
 
-function Main() {
-    return (
-      <>
-        <Container>
-          <Row>
-            {animalData.map((data, key) => {
-              return (
-                <Col xs={12} md={6} lg={4}>
-                  <div key={key} >
-                    <HornedBeast
-                      key={key}
-                      image_url={data.image_url}
-                      title={data.title}
-                      description={data.description}
-                    />  
-                  </div>
-                </Col>
-              );
-            })}
-          </Row>
-        </Container>
-      </>
-      );
-    }
+function Main({animalData}) {
+  return (
+    <>
+      <Container>
+        <Row>
+          {animalData.map((data, key) => {
+            return (
+              <Col xs={12} md={6} lg={4}>
+                <div key={key}>
+                  <HornedBeast
+                    key={key}
+                    image_url={data.image_url}
+                    title={data.title}
+                    description={data.description}
+                  />
+                </div>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    </>
+  );
+}
 
 export default Main;
