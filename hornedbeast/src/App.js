4c619eaefb2414cb8c232a26ba5dcd9a1a8c4fc6
SARA PERRY <SARAPERRYY@GMAIL.COM>
2021-08-09 14:42:40 -0500

    lab04

--- hornedbeast/src/App.js
+++ hornedbeast/src/App.js
@@ -1,20 +1,23 @@
 // import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
-import Main from './components/Main';
 import Footer from './components/Footer';
+import Main from './components/Main';
+import { animalData } from './data';
+// import { useState } from 'react';
 
 
 function App() {
+  
   return (
     <div className="App">
       <Header/>
-      <Main
+      <Main 
         animalData={animalData}
-        />
+      />
       <Footer/>
     </div>
   );
 }
 
-// export default App;
+export default App;
